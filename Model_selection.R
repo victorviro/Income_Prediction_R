ced062e7ae42f51c06f635664ecc16c9422fb939
Victor <48212502+victorviro@users.noreply.github.com>
2019-07-16 14:30:57 +0200

    Plot R-squared added

--- Model_selection.R
+++ Model_selection.R
@@ -3,9 +3,8 @@
 # Model selection
 ##############################
 
-
 #Best Subset Selection
-
+model_selection$bound
 library(leaps)
 
 # nvmax help us to choice how variables as maximum we desire to train our model
@@ -13,6 +12,7 @@ n=19
 model_selection = regsubsets(Income ~ Age + Workclass + Education_Num + Marital_status + Hours_Per_Week +  Education_Num + Marital_status + Occupation + Relationship + Race + Gender + Benefits ,
                               data = data, nvmax = n)
 # We assess the best set of variables for each model size
+model_selection$xnames
 summary_best_models = summary(model_selection)
 summary_best_models$outmat
 # We can also change the method of stepwise selection
@@ -25,33 +25,38 @@ model_selection_backward = regsubsets(Income ~ Age + Workclass + Education_Num +
 
 
 
-# We can also get get the RSS, adjusted R^2 and BIC and Cp
-
+# We can also get get the R-squared, adjusted R-squared, BIC and Cp
+R2 = summary_best_models$rsq
 adj_R2 = summary_best_models$adjr2
 Cp = summary_best_models$cp
 BIC = summary_best_models$bic
 number_variables = 1:n
-dataf = data.frame(number_variables, adj_R2, Cp, BIC )
+dataf = data.frame(number_variables, R2, adj_R2, Cp, BIC)
+
+plot_R2 = ggplot(dataf, aes(x=number_variables, y= R2)) +geom_line( color='brown') + 
+  geom_point(size=2.5) + ggtitle("Plot R-squeared") +
+  xlab("Number of model variables") + ylab("R2")
+plot_R2
 
 # We want high values of adjusted R2
 plot_adj = ggplot(dataf, aes(x=number_variables, y= adj_R2)) +geom_line( color='red') + 
-  geom_point(size=2.5) + ggtitle("Plot adjusted R2") +
-  xlab("Number of variables") + ylab("Adjusted R2")
+  geom_point(size=2.5) + ggtitle("Plot adjusted R-squared") +
+  xlab("Number of model variables") + ylab("Adjusted R2")
 plot_adj
 
 # We want low values of Cp and BIC
 
 plot_cp = ggplot(dataf, aes(x=number_variables, y= Cp)) + geom_line( color= 'green') + 
   geom_point(size=2.5) + ggtitle("Plot Cp") +
-  xlab("Number of variables") + ylab("Cp")
+  xlab("Number of model variables") + ylab("Cp")
 plot_cp
 
 plot_bic = ggplot(dataf, aes(x=number_variables, y= BIC))+ geom_line( color='blue') + 
   geom_point(size=2.5) + ggtitle("Plot BIC") +
-  xlab("Number of variables") + ylab("BIC")
+  xlab("Number of model variables") + ylab("BIC")
 plot_bic
 
-
+plot(model_selection, scale = 'Cp')
 # We see importance of variables
 plot(model_selection, col='darkcyan')
 
@@ -63,5 +68,7 @@ model_selection5 = regsubsets(Income ~ Age + Workclass + Education_Num + Marital
                               data = data, nvmax = n)
 # We assess the best set of variables for each model size
 summary_best_models5 = summary(model_selection5)$outmat
+
+
 
 
