01c207207033e2aa380ad6890005565dfabfbdad
Victor <48212502+victorviro@users.noreply.github.com>
2019-07-18 09:15:55 +0200

    Modified functions threshold

--- Logistic_regresion.R
+++ Logistic_regresion.R
@@ -2,18 +2,19 @@
 # Logistic regression
 ##############################
 
-# We run script depuration_preprocessing to get cleaned dataset 
+# See previously script depuration_preprocessing to get cleaned dataset and selection model script
 
 # We train the model
 
 model_logistic = glm(Income ~ Education_Num + Age + Marital_status + Hours_Per_Week + Benefits,
                         data = data[train,] , family = binomial)
-summary(model_logistic)
+summary_logisitc = summary(model_logistic)
+summary_logisitc
 
-# We get probabilities and convert them in predictions
+# We get probabilities and convert them in predictions using threshold .5
 contrasts(data[train,]$Income)
 prob_train_logistic = predict(model_logistic, type = "response")
-pred_train_logistic = ifelse(prob_train_logistic > .5,'>50K' , "<=50K")
+pred_train_logistic = ifelse(prob_train_logistic > 0.5,'>50K' , "<=50K")
 
 # We do it using test set
 prob_test_logistic = predict(model_logistic, data[test,] , type = "response")
@@ -25,62 +26,66 @@ pred_test_logistic = ifelse(prob_test_logistic > 0.5,'>50K' , "<=50K")
 library(caret)
 cf_train_logistic = caret::confusionMatrix(as.factor(pred_train_logistic), data[train,]$Income)
 cf_train_logistic
-indicators_train_logistic = c(cf_train_logistic$overall[1:2],cf_train_logistic$byClass[c(1,2,5,6)])
+indicators_train_logistic = c(cf_train_logistic$overall[1:1],cf_train_logistic$byClass[c(1,2)])
+indicators_train_logistic
+
 cf_test_logistic = caret::confusionMatrix(as.factor(pred_test_logistic), data[test,]$Income)
 cf_test_logistic
-indicators_test_logistic = c(cf_test_logistic$overall[1:2],cf_test_logistic$byClass[c(1,2,5,6)])
+indicators_test_logistic = c(cf_test_logistic$overall[1:1],cf_test_logistic$byClass[c(1,2)])
+indicators_test_logistic
 
 # We also plot a ROC curve. The area under the curve(AUC) is other measure which help us
-# to choice the best model. How much area does the curve have better
+# to choice the best model. How much area does the curve have higher accuracy will have model
 #https://rpubs.com/Cristina_Gil/Regresion_Logistica
+
+# Using library pROC
 library(pROC)
-library(ggplot2)
-library(grid)
-
-
 roc_logistic = roc(response =data[test,]$Income , predictor = prob_test_logistic)
 plot(roc_logistic)
-
-# we create a function which plot a roc curve with title and colour
-
 roc_logistic
-Curve_ROC = function(roc, name, color){
-  auc = round(auc(roc), digits=5)
-  my_text = paste('Area under the curve:',as.character(auc))
-  roc_plot = ggroc(roc, col= color, cex.lab = 1.5, cex.axis = 1.5, main = "")
-  dev.off()
-  my_grob = grid.text(my_text, x=0.6,  y=0.05, gp=gpar(col="black", fontsize=14, fontface="bold"))
-  roc_plot = roc_plot +
-    geom_abline(slope = 1, intercept = 1, linetype = 'dashed', size = 1)+
-    labs(title="Curve Roc", subtitle= name)+
-    annotation_custom(my_grob)
-  roc_plot
-}
+# we use a function to plot a roc curve with title and colour(created in script Depuration)
 roc_LOG = Curve_ROC(roc_logistic, 'Logistic regression', 'red' )
 roc_LOG
 
-model_logistic$deviance
 
-#influencia_logistic=barplot(abs(summary(model_logistic)$coeff[,3]),horiz=TRUE, col='blue')
-# We graph a plot which we can see the significant variables of the model
-k = summary(model_logistic)$coeff[,3]
-kk = data.frame(k, names(k))
-ocu = rep(FALSE, length(kk[,2]))
-ocu[1:1] = 'Intercept'
-ocu[2:2] = 'Education_Num'
-ocu[3:3] = 'Age'
-ocu[4:7] = 'Marital_status'
-
-plot_influence_logistic = ggplot(kk, aes(kk[,2], abs(kk[,1]), color= ocu ))  +
+# Significativity of variables
+coefficients_logisitc = data.frame(round(summary_logisitc$coefficients, digits = 4))
+Significativity = rep(TRUE, dim(coefficients_logisitc)[1])
+Significativity[coefficients_logisitc$Pr...z..>0.1] = FALSE
+coefficients_logisitc['Significativity'] = Significativity
+coefficients_logisitc
+# We graph a plot which we can see the most significant variables of the model(using values of statist Z instead p-value)
+Z_value = summary_logisitc$coeff[,3]
+Z_value_df = data.frame(Z_value, names(Z_value))
+name_variables =names(Z_value)
+name_variables[4:7] = 'Marital_status'
+plot_significativity_logistic = ggplot(Z_value_df, aes(Z_value_df[,2], abs(Z_value_df[,1]), color= name_variables ))  +
   geom_bar(stat = "identity", position = position_stack(reverse = TRUE)) +
-  coord_flip()+
-  xlab('valiables') + ylab('|Z|') +
+  coord_flip() + xlab('valiables') + ylab('|Z|') +
   geom_hline(yintercept = 1.5, color='gray', size=1)+
   labs(title = "Significativity of variables", subtitle = 'Logistic regression')
-plot_influence_logistic
+plot_significativity_logistic
 
 
-# Now we train some models with diferentnumber of variables(using model selection) 
+
+# We use function thresold_plot(script depuration) to visualize indicators of the model
+# (accuracy,..) for different values of threshold
+thresold_plot_logistic = thresold_plot(roc_logistic)
+thresold_plot_logistic
+
+# We can see that a value 0.55 predict aprox 0.8 both sensitivity and specificity
+# But we can see the values of threshold best accuracy
+coords(roc_logistic, 'best', 'threshold')
+# We use function threshold_table(script depuration)
+threshold_table_logisitic= threshold_table(c(0.4,0.45,0.4941281,0.5, 0.53, 0.57, 0.62), prob_test_logistic)
+threshold_table_logisitic
+# We can improve sensitivity increasing threshold but at the expense of loss 
+# specificity and a little bit accuracy
+
+
+
+
+# Now we train some models with diferent number of variables(using best subset selection) 
 # and then we compare them using anova function
 #http://uc-r.github.io/logistic_regression
 model_logistic10 = glm(Income ~ Education_Num + Age  + Hours_Per_Week + Benefits + Relationship  + Gender + Occupation   ,
@@ -107,40 +112,15 @@ anova_logistic
 
 
 
-# Lets choice a threshold number. We can acces to sensitivity and specificity
-# for different values of threshold.
 
-coord_all= coords(roc_logistic, 'all')
-dataframe1 = data.frame(specificity=coord_all['specificity',],threshold=coord_all['threshold',], sensitivity=coord_all['sensitivity',] )
-thresold_plot_logistic = ggplot(dataframe1,  aes(x=threshold, y= specificity, color='specificity')) + geom_point() +
-                        geom_line(aes(y= sensitivity, color='sensitivity'), size=2) +
-  labs(title="Plot values threshold ", y="specificity/sensitivity", x="threshold")
-thresold_plot_logistic
 
-# We can see that a value 0.55 predict aprox 0.8 both sens and spec
-# But we can see the values of threshold best accuracy
 
-coords(roc_logistic, 'best', 'threshold')
-# We are going to try with values of threshold: 0.5, 0.55, , 0.4547016
-# 0.5
-indicators_test_logistic
-indicators_test_logistic['threshold']=0.5
 
-threshold_table_logisitic=indicators_test_logistic[c(1,3,4,7)]
-for (i in c(0.38, 0.4585140, 0.53, 0.57, 0.60)){
-  pred_test_logistic_threshold = ifelse(prob_test_logistic > i ,'>50K' , "<=50K")
-  cf_test_logistic_threshold = caret::confusionMatrix(as.factor(pred_test_logistic_threshold), data[test,]$Income)
-  cf_test_logistic_threshold
-  indicators_test_logistic_threshold = c(cf_test_logistic_threshold$overall[1:1],cf_test_logistic_threshold$byClass[c(1,2)])
-  indicators_test_logistic_threshold['threshold']=i
-  
-  threshold_table_logisitic=rbind(threshold_table_logisitic, indicators_test_logistic_threshold)
-}
-rownames(threshold_table_logisitic) = 1:nrow(threshold_table_logisitic)
-threshold_table_logisitic
 
-# We can improve sensitivity increasing threshold but a costa to loss 
-# specificiti and a little bit accuracy
+
+
+
+
 
 
 
